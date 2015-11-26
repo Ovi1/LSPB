6389d13ea87b019974613df38c41a6f872371513
Ovi1 <ovidijus.baublys@gmail.com>
2015-11-26 22:05:12 +0200

    dashbord, home, navbar table template changes new position veriables

--- index.php
+++ index.php
@@ -1,31 +1,23 @@
+
 <?php
-
-
 //No Direct Access
 defined('_JEXEC') or die;
-
 //Include Logic
 include('logic.php');
 ?>
 <!DOCTYPE html>
+<html>
 <head>
-	<meta charset="utf-8">
+  <!-- Joomla Head -->
+	<jdoc:include type="head" />
+<!--  
+	<meta charset="utf-8">-->
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 	<meta name="viewport" content="width=device-width,initial-scale=1">
 
-	<!-- Joomla Head -->
-	<jdoc:include type="head" />
-
-    <!-- Bootstrap core CSS -->
-    <!--<link href="<?php echo $this->baseurl; ?>/templates/<?php echo $this->template; ?>/css/bootstrap.css" rel="stylesheet">-->
-     <!-- Justified-Nav CSS -->
-	<link href="<?php echo $this->baseurl; ?>/templates/<?php echo $this->template; ?>/css/jumbotron.css" rel="stylesheet">
-    <!-- Font Awesome CSS -->
-    <link href="<?php echo $this->baseurl; ?>/templates/<?php echo $this->template; ?>/font-awesome.css" rel="stylesheet">
-    <!-- Custom CSS -->
+  <!-- Custom CSS -->
 	<link href="<?php echo $this->baseurl; ?>/templates/<?php echo $this->template; ?>/css/style.css" rel="stylesheet">
  </head>
-
 <body>
    <!-- Menu Module -->
    <jdoc:include type="modules" name="menu" style="none" />
@@ -40,9 +32,11 @@ include('logic.php');
         <?php echo (isset($sidebar) ? '<div class="col-md-8">' : '<div class="col-md-12">'); ?>
             <!-- Messages -->
             <jdoc:include type="message" />
+            
             <?php if($show_frontpage_component == 0 && $menu->getActive() == $menu->getDefault()) : ?>
                 <!-- Do not show component -->
             <?php else : ?>
+                
                 <!-- Main Component -->
                 <jdoc:include type="component" />
             <?php endif; ?>
@@ -54,8 +48,13 @@ include('logic.php');
                 <jdoc:include type="modules" name="sidebar" style="xhtml" />
             </div>
         <?php endif; ?>
-    </div><!--./ row -->
-
+        
+        
+            <!-- Content -->
+            <div class="col-md-12">
+                <jdoc:include type="modules" name="content" style="xhtml" />
+            </div>
+        
     <?php if($this->countModules('box1') || $this->countModules('box2') || $this->countModules('box3')) : ?>
       <!-- Example row of columns -->
       <div class="row">
@@ -76,25 +75,24 @@ include('logic.php');
         <?php endif; ?>
       </div>
     <?php endif; ?>
+      </div> <!-- /container -->
+      <div class="container-fluid">
+        <footer class="footer">
+          <p><?php echo $copyright; ?></p>
+        </footer>
+      </div>
 
-      <hr>
-
-      <footer>
-        <p><?php echo $copyright; ?></p>
-      </footer>
-    </div> <!-- /container -->
-
- <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-	<script defer src=https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-	<script defer src="templates/<?php echo $this->template ?>/js/bootstrap.js"></script>
-
-<!--	<script defer src="templates/<?php echo $this->template ?>/js/script.js"></script>-->
+  <!--jQuery (necessary for Bootstrap's JavaScript plugins)--> 
+  
+<!--<script defer src=<?php echo $this->template ?>templates/js/jquery.js"></script>-->
+  
+  <!-- Latest compiled and minified JavaScript 
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+  
+  <!--<script defer src="templates/<?php echo $this->template ?>/js/script.js"></script>-->
 
 	<!-- end scripts-->
-
-
-
-
+  
 	<!--[if lt IE 7 ]>
 		<script src="//ajax.googleapis.com/ajax/libs/chrome-frame/1.0.3/CFInstall.min.js"></script>
 		<script>window.attachEvent('onload',function(){CFInstall.check({mode:'overlay'})})</script>
